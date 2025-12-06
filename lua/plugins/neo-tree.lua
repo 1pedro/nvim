526dfa99da17856e8a3d1d4ebdb89f22e5eeac3f
Pedro Jefferson <pedro.nsilva@nubank.com.br>
2025-12-05 21:04:27 -0300

    fix neo-tree explorer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,15 @@
 return {
-  "neo-tree.nvim",
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    { "<leader>e", ":Neotree toggle reveal<CR>", desc = "NeoTree Reveal (Root Dir)", remap = true },
+    { "<leader>E", ":Neotree toggle reveal_force_cwd<CR>", desc = "NeoTree Reveal (cwd)", remap = true },
+  },
   opts = {
     filesystem = {
       filtered_items = {
         visible = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
       },
     },
   },
