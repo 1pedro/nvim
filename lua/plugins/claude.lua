54c56d288b9638cc0a5e1468d504d5f86e359299
Pedro Sales <pedrojefferson.dev@gmail.com>
2026-02-23 10:09:23 -0300

    add claude plugin

---
+++ lua/plugins/claude.lua
@@ -0,0 +1,11 @@
+return {
+  "coder/claudecode.nvim",
+  dependencies = { "folke/snacks.nvim" },
+  opts = {
+    terminal_cmd = "/Users/pedro/.local/bin/claude", -- Point to local installation
+  },
+  config = true,
+  keys = {
+    -- Your keymaps here
+  },
+}
